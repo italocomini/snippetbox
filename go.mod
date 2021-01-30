90a883d48a9c41af490b29171af4a90878205f80
Italo Comini <italocomini@gmail.com>
2021-01-30 14:58:23 -0300

    commit inicial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/italocomini/snippetbox
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If it doesn't, use
+	// the http.NotFound() function to send a 404 response to the client.
+	// Importantly, we then return from the handler. If we don't return the handler
+	// would keep executing and also write the "Hello from SnippetBox" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a showSnippet handler function.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// Use the fmt.Fprintf() function to interpolate the id value with our response
+	// and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
+}
+
+// Add a createSnippet handler function.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(405)
+		// w.Write([]byte("Methods Not Allowed"))
+		// OU
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", 405)
+
+		return
+	}
+
+	// Set a new cache-control header. If an existing "Cache-Control" header exists
+	// it will be overwritten.
+	w.Header().Set("Cache-Control", "public, max-age=31536000")
+	// OR
+	// In contrast, the Add() method appends a new "Cache-Control" header and can
+	// be called multiple times.
+	// w.Header().Add("Cache-Control", "public")
+	// w.Header().Add("Cache-Control", "max-age=31536000")
+	//
+	// Delete all values for the "Cache-Control" header.
+	// w.Header().Del("Cache-Control")
+	// Retrieve the first value for the "Cache-Control" header.
+	// w.Header().Get("Cache-Control")
+	w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"name":"Alex"}`))
+}
+
+// Main
+func main() {
+	// Register the two new handler functions and corresponding URL patterns with
+	// the servemux, in exactly the same way that we did before.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	// OR
+	// http.HandleFunc("/", home)
+	// http.HandleFunc("/snippet", showSnippet)
+	// http.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
