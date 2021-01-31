e039476e03e53ff600f061373710dca35d41f4ee
Italo Comini <italocomini@gmail.com>
2021-01-31 15:30:22 -0300

    cap 3.2 - managing configuration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"strings"
 )
 
 func main() {
+
+	// Define a new command-line flag with the name 'addr', a default value of ":4000"
+	// and some short help text explaining what the flag controls. The value of the
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line flag.
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any errors are
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -22,9 +36,18 @@ func main() {
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	// The value returned from the flag.String() function is a pointer to the flag
+	// value, not the value itself. So we need to dereference the pointer (i.e.
+	// prefix it with the * symbol) before using it. Note that we're using the
+	// log.Printf() function to interpolate the address with the log message.
+	log.Printf("Starting server on %s", *addr)
+
+	// Ports 0-1023 are restricted and (typically) can only be used by services which
+	// have root privileges. If you try to use one of these ports you should get a
+	// bind: permission denied error message on start-up
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
+
 }
 
 // Antes de usar esse código, é perciso ler o artigo no site do Alex
