2381c001afb8b833163d25e33f0e5c25bc6150fb
Italo Comini <italocomini@gmail.com>
2021-02-07 11:25:17 -0300

    cap 5.5 - Common Dynamic Data

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,7 +5,20 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
+
+// Create an addDefaultData helper. This takes a pointer to a templateData
+// struct, adds the current year to the CurrentYear field, and then returns
+// the pointer. Again, we're not using the *http.Request parameter at the
+// moment, but we will do later in the book.
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
 
 // The serverError helper writes an error message and stack trace to the errorLog,
 // then sends a generic 500 Internal Server Error response to the user.
@@ -40,7 +53,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	// Write the template to the buffer, instead of straight to the
 	// http.ResponseWriter. If there's an error, call our serverError helper and then
 	// return.
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
