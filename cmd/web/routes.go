b8ae293bdc71e7cff12394507eb7828f74418234
Italo Comini <italocomini@gmail.com>
2021-02-08 10:48:16 -0300

    cap 6.4 - Panic Recovery

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,7 +22,7 @@ func (app *application) routes() http.Handler {
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
-	return app.logRequest(secureHeader(mux))
+	return app.recoverPanic(app.logRequest(secureHeader(mux)))
 
 }
 
