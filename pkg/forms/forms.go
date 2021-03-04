345b551563ae00d11dc99f40fe4d048b78bf3393
Italo Comini <italocomini@gmail.com>
2021-03-04 11:36:01 -0300

    cap 11.3 e 4 - user login

--- pkg/forms/forms.go
+++ pkg/forms/forms.go
@@ -3,13 +3,42 @@ package forms
 import (
 	"fmt"
 	"net/url"
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
 
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
 type Form struct {
 	url.Values
 	Errors errors
+}
+
+// Implement a MinLength method to check that a specific field in the form
+// contains a minimum number of characters. If the check fails then add the
+// appropriate message to the form errors.
+func (f *Form) MinLength(field string, d int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if utf8.RuneCountInString(value) < d {
+		f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum is %d characters)", d))
+	}
+}
+
+// Implement a MatchesPattern method to check that a specific field in the form
+// matches a regular expression. If the check fails then add the
+// appropriate message to the form errors.
+func (f *Form) MatchesPattern(field string, pattern *regexp.Regexp) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if !pattern.MatchString(value) {
+		f.Errors.Add(field, "This field is invalid")
+	}
 }
 
 func New(data url.Values) *Form {
